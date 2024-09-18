51f02c8f973f820f33fb157f1228d3f44c312c4f
AlecMalenfant001 <malenfantalec@gmail.com>
2024-09-18 11:15:29 -0500

    Create getimg.ai api test button

---
+++ components/test-getimg.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+export default function GetImgTestButton({ promptText }) {
+  async function fetchImageFromGetImgAI() {
+    const apiKey = import.meta.env.VITE_GETIMG_API_KEY;
+    const url = "https://api.getimg.ai/v1/stable-diffusion-xl/text-to-image";
+    const body = JSON.stringify({
+      prompt: "a cat in a hat",
+      width: 1024,
+      height: 1024,
+      output_format: "png",
+      response_format: "url",
+      model: "stable-diffusion-xl-v1-0",
+    });
+
+    const options = {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        Authorization: `Bearer ${apiKey}`,
+        "Content-Type": "application/json",
+      },
+      body,
+    };
+
+    try {
+      // Make the API request
+      const response = await fetch(url, options);
+
+      // Handle non-2xx status codes
+      if (!response.ok) {
+        throw new Error(`API request failed with status ${response.status}`);
+      }
+      // Parse the JSON response
+      const data = await response.json();
+      console.log("image url: ", data.url);
+    } catch (error) {
+      console.error("Error fetching image:", error);
+      return null; // Or handle the error differently
+    }
+  }
+
+  return <button onClick={fetchImageFromGetImgAI}>Test Getimg API</button>;
+}
